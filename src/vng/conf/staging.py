93eef6af4004ccbee6ba573a872c64a4227e6cbc
Alvise <alvise@maykinmedia.nl>
2018-11-22 11:22:34 +0100

    staging files for ansible 2

--- src/vng/conf/staging.py
+++ src/vng/conf/staging.py
@@ -16,8 +16,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'vng-staging',
-        'USER': 'vng',
-        'PASSWORD': 'vng',
+        'USER': 'vng-staging',
+        'PASSWORD': 'vng-staging',
         'HOST': '',  # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',  # Set to empty string for default.
     }

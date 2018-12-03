9d1890b48dc49e37bc5860ecb2829b3772b84880
Alvise <alvise@maykinmedia.nl>
2018-12-03 16:48:05 +0100

    Added test frontend

--- src/vng/servervalidation/tests/test_frontend.py
+++ src/vng/servervalidation/tests/test_frontend.py
@@ -1,9 +1,9 @@
+import factory
 from django_webtest import WebTest
 from django.urls import reverse
 from factory.django import DjangoModelFactory as Dmf
-import factory
 from .test_factory import TestScenarioFactory, ServerRunFactory
-
+from vng.servervalidation.models import ServerRun
 
 class TestCreation(WebTest):
 
@@ -42,6 +42,20 @@ class TestList(WebTest):
     def test_list(self):
         call = self.app.get('/server/server-run_list',user='test')
         assert 'no session' not in str(call.body)
+
+    def test_run_test(self):
+        call = self.app.get('/server/start_server-run',user='test')
+        form = call.forms[1]
+        form['test_scenario'].force_value('1')
+        form['api_endpoint'].force_value('https://google.com')
+        form.submit()
+        sr = ServerRun.objects.latest('pk')
+
+        call = self.app.get('/server/server-run_detail/{}/log'.format(sr.pk), user='test')
+
+
+
+
 
 
 

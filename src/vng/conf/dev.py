2a6edfffb9b86d7b2b20079974e08179aafde27e
Alvise <alvise@maykinmedia.nl>
2018-12-04 14:22:41 +0100

    base py

--- src/vng/conf/dev.py
+++ src/vng/conf/dev.py
@@ -74,11 +74,6 @@ ENVIRONMENT = 'development'
 # Library settings
 #
 
-# Django debug toolbar
-INSTALLED_APPS += [
-    'debug_toolbar',
-]
-
 
 INTERNAL_IPS = ('127.0.0.1',)
 DEBUG_TOOLBAR_CONFIG = {

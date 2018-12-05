cababaa400dbfc401468353f1192db8c2836a136
Sven van de Scheur <svenvandescheur@gmail.com>
2018-12-05 17:51:45 +0100

    :bug: - & -> $

--- src/vng/testsession/urls.py
+++ src/vng/testsession/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('&', views.SessionListView.as_view(), name='sessions'),
+    url('$', views.SessionListView.as_view(), name='sessions'),
     url('(?P<session_id>[0-9]+)', views.SessionLogView.as_view(), name='session_log'),
     url('(?P<session_id>[0-9]+)/stop', login_required(views.stop_session), name='stop_session'),
     url(r'runtest/(?P<url>([^/])+)/(?P<relative_url>(.)*)$', views.RunTest.as_view(), name='sessionTypes'),

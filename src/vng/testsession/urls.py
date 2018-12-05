fbca7d1345314b6d25d26766fa0a6279a625889a
Alvise <alvise@maykinmedia.nl>
2018-12-05 17:19:39 +0100

    Mergin in a unique page sessions

--- src/vng/testsession/urls.py
+++ src/vng/testsession/urls.py
@@ -6,7 +6,6 @@ from . import views
 
 urlpatterns = [
     url('sessions/', views.SessionListView.as_view(), name='sessions'),
-    url('start-session/', views.SessionCreate.as_view(), name='start_session'),
     url('session/(?P<session_id>[0-9]+)/log', views.SessionLogView.as_view(), name='session_log'),
     url('stop-session/(?P<session_id>[0-9]+)', login_required(views.stop_session), name='stop_session'),
     url(r'runtest/(?P<url>([^/])+)/(?P<relative_url>(.)*)$', views.RunTest.as_view(), name='sessionTypes'),

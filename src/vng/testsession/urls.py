e59ecffdbe104ca197ac6e4ee1561fa27e334021
Alvise <alvise@maykinmedia.nl>
2018-12-05 16:48:30 +0100

    Proxy View

--- src/vng/testsession/urls.py
+++ src/vng/testsession/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url, include
 from rest_framework import routers, serializers, viewsets
+from django.contrib.auth.decorators import login_required
 from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     url('sessions/', views.SessionListView.as_view(), name='sessions'),
     url('start-session/', views.SessionCreate.as_view(), name='start_session'),
-    url('stop-session/(?P<session_id>[0-9]+)', views.stop_session, name='stop_session'),
+    url('session/(?P<session_id>[0-9]+)/log', views.SessionLogView.as_view(), name='session_log'),
+    url('stop-session/(?P<session_id>[0-9]+)', login_required(views.stop_session), name='stop_session'),
     url(r'runtest/(?P<url>([^/])+)/(?P<relative_url>(.)*)$', views.RunTest.as_view(), name='sessionTypes'),
 ]

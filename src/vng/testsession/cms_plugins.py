45b375afc2d1d8f655a6a23f70914caf45aefd54
Alvise <alvise@maykinmedia.nl>
2018-12-05 17:13:53 +0100

    Merge of new server in one page

--- src/vng/testsession/cms_plugins.py
+++ src/vng/testsession/cms_plugins.py
@@ -2,7 +2,6 @@ from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from cms.models.pluginmodel import CMSPlugin
 from django.utils.translation import ugettext_lazy as _
-from vng.testsession.models import TextPluginModel
 
 @plugin_pool.register_plugin
 class HelloPlugin(CMSPluginBase):

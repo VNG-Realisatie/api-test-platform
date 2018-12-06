5d3af2d9ea2a7d01f2c586a50e42ffc0da60086d
Sven van de Scheur <svenvandescheur@gmail.com>
2018-12-06 11:02:47 +0100

    :sparkles: - Add some minimal validation to test scenario json.

--- src/vng/utils/views.py
+++ src/vng/utils/views.py
@@ -1,7 +1,10 @@
 from django import http
+from django.http import Http404
 from django.template import loader, TemplateDoesNotExist
 from django.views.defaults import ERROR_500_TEMPLATE_NAME
 from django.views.decorators.csrf import requires_csrf_token
+from django.views.generic.edit import ModelFormMixin, ProcessFormView
+from django.views.generic.list import MultipleObjectMixin, MultipleObjectTemplateResponseMixin
 
 
 @requires_csrf_token
@@ -21,3 +24,33 @@ def server_error(request, template_name=ERROR_500_TEMPLATE_NAME):
         return http.HttpResponseServerError('<h1>Server Error (500)</h1>', content_type='text/html')
     context = {'request': request}
     return http.HttpResponseServerError(template.render(context))
+
+
+
+class ListAppendView(MultipleObjectMixin, MultipleObjectTemplateResponseMixin, ModelFormMixin, ProcessFormView):
+    """
+    A View that displays a list of objects and a form to create a new object.
+    The View processes this form.
+    """
+    template_name_suffix = '_append'
+    allow_empty = True
+
+    def get(self, request, *args, **kwargs):
+        self.object_list = self.get_queryset()
+        allow_empty = self.get_allow_empty()
+        if not allow_empty and len(self.object_list) == 0:
+            raise Http404(_(u"Empty list and '%(class_name)s.allow_empty' is False.")
+                          % {'class_name': self.__class__.__name__})
+        self.object = None
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        context = self.get_context_data(object_list=self.object_list, form=form)
+        return self.render_to_response(context)
+
+    def post(self, request, *args, **kwargs):
+        self.object = None
+        return super(ListAppendView, self).post(request, *args, **kwargs)
+
+    def form_invalid(self, form):
+        self.object_list = self.get_queryset()
+        return self.render_to_response(self.get_context_data(object_list=self.object_list, form=form))

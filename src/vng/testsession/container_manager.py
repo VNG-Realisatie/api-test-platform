7290d094c4ad43862ed2a0b271550487a5af1376
Alvise <alvise@maykinmedia.nl>
2018-12-07 16:55:15 +0100

    :recycle: command lie splitted and added function in a separate file

--- src/vng/testsession/container_manager.py
+++ src/vng/testsession/container_manager.py
@@ -1,22 +1,10 @@
-import subprocess
-import logging
-import re
-
-logger = logging.getLogger(__name__)
-
-
-def runcommand(command):
-    logger.debug('running the COMMAND: {}'.format(command))
-    subp = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    err = subp.stderr.read()
-    if len(err) > 1:
-        logger.error(err)
-    else:
-        err = None
-    return (subp.stdout.read(), err)
+from ..utils.commands import run_command
 
 
 class ContainerManagerHelper():
+    '''
+    Not used yet but it can be used in order to get a feedback by the server
+    '''
 
     def __init__(self, cluster, zone="europe-west4-a"):
         self.cluster = cluster
@@ -24,39 +12,50 @@ class ContainerManagerHelper():
 
     def poolList(self):
         COMMAND = "gcloud container node-pools list --cluster={} --zone={}".format(self.cluster, self.zone)
-        res = runcommand(COMMAND)
+        res = run_command(COMMAND)
         return res
 
     def createPool(self, pool_name):
         COMMAND = "gcloud container node-pools create {} --cluster={} --zone={}".format(pool_name, self.cluster, self.zone)
-        res = runcommand(COMMAND)
+        res = run_command(COMMAND)
         return res
 
     def deletePool(self, pool_name):
         COMMAND = "gcloud container node-pools delete {} --cluster={} --zone={} -q".format(pool_name, self.cluster, self.zone)
-        res = runcommand(COMMAND)
+        res = run_command(COMMAND)
         return res
 
 
 class K8S():
 
     def deploy(self, app_name, image, port=None):
-        if port is None:
-            COMMAND = 'kubectl run {} --image={} --env="DOMAIN=cluster"'.format(app_name, image)
-        else:
-            COMMAND = 'kubectl run {} --image={} --port={} --env="DOMAIN=cluster"'.format(app_name, image, port)
-        res = runcommand(COMMAND)
+
+        COMMAND = ['kubectl',
+                   'run',
+                   app_name,
+                   '--image=',
+                   image,
+                   '--env="DOMAIN=cluster"']
+        if port is not None:
+            COMMAND += [
+                'port',
+                port
+            ]
+        res = run_command(COMMAND)
         return res
 
     def delete(self, app_name):
         COMMAND = 'kubectl delete -n default deployment {}'.format(app_name)
-        res1 = runcommand(COMMAND)
+        res1 = run_command(COMMAND)
         return res1
 
     def status(self, app_name):
         NAMES = ['namespace', 'desired', 'current', 'up-to-date', 'available', 'age']
-        COMMAND = 'kubectl get deployments --all-namespaces'
-        res1 = runcommand(COMMAND)[0].decode('utf-8')
+        COMMAND = ['kubectl',
+                   'get',
+                   'deployments',
+                   '--all-namespaces']
+        res1 = run_command(COMMAND)[0].decode('utf-8')
         reg = re.search(r'(\S*) *{} *(\S*) *(\S*) *(\S*) *(\S*) *(\S*) *\n'.format(app_name), res1, re.M)
         if reg:
             status = {'app_name': app_name}
---
+++ src/vng/utils/commands.py
@@ -0,0 +1,21 @@
+import subprocess
+import logging
+import re
+
+from django.conf import settings
+from .errors import CommandError
+
+logger = logging.getLogger(__name__)
+
+
+def run_command(command):
+
+    if settings.RUN_KUBERNETES_CMD:
+        subp = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, err = subp.communicate('n\n')
+        if err:
+            logger.debug('running the COMMAND: {}'.format(command))
+            logger.exception(err.decode('utf-8'))
+            raise CommandError(err.decode('utf-8'), command)
+    else:
+        return 'success'

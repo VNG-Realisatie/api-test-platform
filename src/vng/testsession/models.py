4f401e58de6d8fd2de13fa2e2cceafe8e75a3652
Alvise <alvise@maykinmedia.nl>
2018-12-07 16:14:23 +0100

    :recycle: status choices in another file

--- src/vng/testsession/models.py
+++ src/vng/testsession/models.py
@@ -2,8 +2,8 @@ import uuid
 from django.db import models
 from django.utils import timezone
 from django.core.files import File
-from djchoices import DjangoChoices, ChoiceItem
 from vng.accounts.models import User
+from ..utils import choices
 
 
 class SessionType(models.Model):
@@ -15,16 +15,12 @@ class SessionType(models.Model):
 
 
 class Session(models.Model):
-    class StatusChoices(DjangoChoices):
-        starting = ChoiceItem("starting")
-        running = ChoiceItem("running")
-        stopped = ChoiceItem("stopped")
 
     name = models.CharField(max_length=20, unique=True, null=True)
     session_type = models.ForeignKey(SessionType, on_delete=models.SET_NULL, null=True)
     started = models.DateTimeField(default=timezone.now)
     stopped = models.DateTimeField(null=True, blank=True)
-    status = models.CharField(max_length=10, choices=StatusChoices.choices, default=StatusChoices.starting)
+    status = models.CharField(max_length=10, choices=choices.StatusChoices.choices, default=choices.StatusChoices.starting)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     api_endpoint = models.URLField(max_length=200, blank=True, null=True, default=None)
     exposed_api = models.CharField(max_length=200, unique=True, null=True)
@@ -41,13 +37,13 @@ class Session(models.Model):
             return "{}".format(self.session_type)
 
     def is_stopped(self):
-        return self.status == self.StatusChoices.stopped
+        return self.status == choices.StatusChoices.stopped
 
     def is_running(self):
-        return self.status == self.StatusChoices.running
+        return self.status == choices.StatusChoices.running
 
     def is_starting(self):
-        return self.status == self.StatusChoices.starting
+        return self.status == choices.StatusChoices.starting
 
 
 class SessionLog(models.Model):

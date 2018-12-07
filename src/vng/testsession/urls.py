be5211efad6cb3a0be4c32fcf8d81477a5ea3bc0
Alvise <alvise@maykinmedia.nl>
2018-12-07 16:00:33 +0100

    :lock: login requirements in the class

--- src/vng/testsession/urls.py
+++ src/vng/testsession/urls.py
@@ -6,7 +6,7 @@ from . import views
 
 urlpatterns = [
     url('^$', views.SessionListView.as_view(), name='sessions'),
-    url('(?P<session_id>[0-9]+)/stop', login_required(views.StopSession.as_view()), name='stop_session'),
+    url('(?P<session_id>[0-9]+)/stop', views.StopSession.as_view(), name='stop_session'),
     url(r'runtest/(?P<url>([^/])+)/(?P<relative_url>(.)*)$', views.RunTest.as_view(), name='sessionTypes'),
     url('(?P<session_id>[0-9]+)', views.SessionLogView.as_view(), name='session_log'),
 ]

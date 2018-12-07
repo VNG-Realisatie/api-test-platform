e4adb4685c3ae568702a7ce9a6f3ebe9ac0079b0
Alvise <alvise@maykinmedia.nl>
2018-12-07 16:17:25 +0100

    removed all fields in serializers

--- src/vng/servervalidation/serializers.py
+++ src/vng/servervalidation/serializers.py
@@ -1,12 +1,14 @@
 from .models import *
 from rest_framework import serializers
 
+
 class TestScenarioSerializer(serializers.ModelSerializer):
     class Meta:
         model = TestScenario
-        fields = '__all__'
+        fields = ['name', 'validation_file']
+
 
 class ServerRunSerializer(serializers.ModelSerializer):
     class Meta:
         model = ServerRun
-        fields = ['pk','test_scenario', 'api_endpoint', 'started', 'stopped']
+        fields = ['pk', 'test_scenario', 'api_endpoint', 'started', 'stopped']

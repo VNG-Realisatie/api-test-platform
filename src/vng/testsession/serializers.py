f363401ef2e39c7d8d26e27af3913dc70a8be0c6
Alvise <alvise@maykinmedia.nl>
2018-12-07 16:17:25 +0100

    removed all fields in serializers

--- src/vng/testsession/serializers.py
+++ src/vng/testsession/serializers.py
@@ -1,12 +1,14 @@
 from .models import *
 from rest_framework import serializers
 
+
 class SessionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Session
-        fields = ('pk','session_type','started','stopped','status','api_endpoint')
+        fields = ['pk', 'session_type', 'started', 'stopped', 'status', 'api_endpoint']
+
 
 class SessionTypesSerializer(serializers.ModelSerializer):
     class Meta:
         model = SessionType
-        fields = '__all__'
+        fields = ['name', 'docker_image']

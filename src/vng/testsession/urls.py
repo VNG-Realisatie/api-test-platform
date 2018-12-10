29fabd6c0997d0594c09b0fa25f5ec10e233279f
Alvise <alvise@maykinmedia.nl>
2018-12-10 11:22:35 +0100

    :recycle: refacoring mixins

--- src/vng/testsession/urls.py
+++ src/vng/testsession/urls.py
@@ -7,6 +7,7 @@ from . import views
 urlpatterns = [
     url('^$', views.SessionListView.as_view(), name='sessions'),
     url('(?P<session_id>[0-9]+)/stop', views.StopSession.as_view(), name='stop_session'),
+    url('(?P<session_id>[0-9]+)/report', views.SessionReport.as_view(), name='session_report'),
     url(r'runtest/(?P<url>([^/])+)/(?P<relative_url>(.)*)$', views.RunTest.as_view(), name='run_test'),
     url('(?P<session_id>[0-9]+)', views.SessionLogView.as_view(), name='session_log'),
 ]

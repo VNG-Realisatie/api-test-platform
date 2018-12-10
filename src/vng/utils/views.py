aa04f283b4ec301229fee1f650e7e3dd72628276
Alvise <alvise@maykinmedia.nl>
2018-12-10 13:01:26 +0100

    Fixed reverse foreign key issue

--- src/vng/utils/views.py
+++ src/vng/utils/views.py
@@ -6,7 +6,7 @@ from django.views.defaults import ERROR_500_TEMPLATE_NAME
 from django.views.decorators.csrf import requires_csrf_token
 from django.views.generic.edit import ModelFormMixin, ProcessFormView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic.list import MultipleObjectMixin, MultipleObjectTemplateResponseMixin
+from django.views.generic.list import MultipleObjectMixin, MultipleObjectTemplateResponseMixin, ListView
 from django.views.generic.detail import SingleObjectMixin, DetailView
 
 
@@ -75,17 +75,52 @@ class OwnerObjectMixin(LoginRequiredMixin):
 
 class SingleOwnerObject(DetailView):
 
-    def get_object(self, queryset):
+    def get_object(self, obj):
         if not self.field_name:
             raise Exception('Field "field_name" in subclasses has not been defined')
 
-        if queryset[self.field_name].user != self.request.user:
+        if getattr(obj, self.field_name).user != self.request.user:
             return HttpResponse('Unauthorized', status=401)
         else:
-            return queryset
+            return obj
 
     def get(self, request, *args, **kwargs):
         queryset = self.get_queryset()
         self.object = self.get_object(queryset)
         context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
+
+
+class OwnerMultipleObjects(ListView):
+
+    def check_ownership(self, queryset):
+        print(queryset)
+        if not self.field_name:
+            raise Exception('Field "field_name" in subclasses has not been defined')
+        params = {
+            self.field_name: self.request.user
+        }
+        return queryset.filter(**params).distinct()
+        # for obj in queryset:
+        #     if getattr(obj, self.field_name).user != self.request.user:
+        #         return HttpResponse('Unauthorized', status=401)
+
+    def get(self, request, *args, **kwargs):
+        self.object_list = self.get_queryset()
+        self.check_ownership(self.object_list)
+        allow_empty = self.get_allow_empty()
+
+        if not allow_empty:
+            # When pagination is enabled and object_list is a queryset,
+            # it's better to do a cheap query than to load the unpaginated
+            # queryset in memory.
+            if self.get_paginate_by(self.object_list) is not None and hasattr(self.object_list, 'exists'):
+                is_empty = not self.object_list.exists()
+            else:
+                is_empty = not self.object_list
+            if is_empty:
+                raise Http404(_("Empty list and '%(class_name)s.allow_empty' is False.") % {
+                    'class_name': self.__class__.__name__,
+                })
+        context = self.get_context_data()
         return self.render_to_response(context)

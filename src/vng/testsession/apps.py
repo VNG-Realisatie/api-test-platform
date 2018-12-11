387c37c7efb5b97a750866d8e999ed21fc810b0e
Alvise <alvise@maykinmedia.nl>
2018-12-11 15:14:11 +0100

    Creation folder startup

--- src/vng/testsession/apps.py
+++ src/vng/testsession/apps.py
@@ -1,5 +1,11 @@
+import os
+
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class TestsessionConfig(AppConfig):
-    name = 'testsession'
+    name = 'vng.testsession'
+
+    def ready(self):
+        pass

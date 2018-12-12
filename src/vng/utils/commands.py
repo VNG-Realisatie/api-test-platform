64ba58d9a98ef3e4590d61313c806a943369d6fe
Alvise <alvise@maykinmedia.nl>
2018-12-12 18:13:33 +0100

    subproc

--- src/vng/utils/commands.py
+++ src/vng/utils/commands.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 def run_command(command):
     logger.debug('running the COMMAND: {}'.format(command))
-    subp = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    subp = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
     res, error = subp.communicate('n\n')
     if error:
         logger.exception(error)

f4453b1851af6396a3667b51bd3ab3d64e2ea478
Alvise <alvise@maykinmedia.nl>
2018-12-17 17:03:20 +0100

    Fixes after the changes of the models

--- src/vng/testsession/tests/factories.py
+++ src/vng/testsession/tests/factories.py
@@ -2,7 +2,7 @@ from django.utils import timezone
 import factory
 from factory.django import DjangoModelFactory as Dmf
 from vng.accounts.models import User
-from ..models import SessionType, Session, Scenario, ScenarioCase
+from ..models import SessionType, Session, ScenarioCase, VNGEndpoint
 from ...utils import choices
 
 
@@ -12,7 +12,20 @@ class SessionTypeFactory(Dmf):
         model = SessionType
 
     name = factory.sequence(lambda n: 'testype %d' % n)
-    docker_image = 'di'
+    standard = 'Stardard'
+    role = 'Role'
+    application = 'Application'
+    version = '1.2.4'
+
+
+class VNGEndpointFactory(Dmf):
+
+    class Meta:
+        model = VNGEndpoint
+
+    name = 'DRC'
+    url = 'http://ref.tst.vng.cloud/drc/api/v1'
+    session_type = factory.SubFactory(SessionTypeFactory)
 
 
 class UserFactory(Dmf):
@@ -24,25 +37,14 @@ class UserFactory(Dmf):
     password = factory.PostGenerationMethodCall('set_password', 'pippopippo')
 
 
-class ScenarioFactory(Dmf):
-
-    class Meta:
-        model = Scenario
-
-    standard = 'Stardard'
-    role = 'Role'
-    application = 'Application'
-    version = '1.2.4'
-
-
 class ScenarioCaseFactory(Dmf):
 
     class Meta:
         model = ScenarioCase
 
     url = 'unknown/23'
-    HTTP_method = choices.HTTPMethodChoiches.GET
-    scenario = factory.SubFactory(ScenarioFactory)
+    http_method = choices.HTTPMethodChoiches.GET
+    vng_endpoint = factory.SubFactory(VNGEndpointFactory)
 
 
 class SessionFactory(Dmf):
@@ -56,4 +58,4 @@ class SessionFactory(Dmf):
     user = factory.SubFactory(UserFactory)
     api_endpoint = 'https://reqres.in/api/'
     exposed_api = 'tst'
-    scenario = factory.SubFactory(ScenarioFactory)
+    session_type = factory.SubFactory(SessionTypeFactory)

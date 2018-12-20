f229f4024ae79b0bc805528086bf514ddf354313
Joeri Bekker <joeri@maykinmedia.nl>
2018-12-20 11:46:44 +0000

    :bug: Fixing missing mandatory data on FK

--- src/vng/testsession/migrations/0034_auto_20181217_1236.py
+++ src/vng/testsession/migrations/0034_auto_20181217_1236.py
@@ -5,14 +5,24 @@ from __future__ import unicode_literals
 from django.db import migrations, models
 import django.db.models.deletion
 
+def forwards_func(apps, schema_editor):
+    VNGEndpoint = apps.get_model("testsession", "VNGEndpoint")
+    SessionType = apps.get_model("testsession", "SessionType")
+    if not VNGEndpoint.objects.filter(id=1):
+        VNGEndpoint.objects.create(name='Example', url='https://vng.nl', session_type=SessionType.objects.first())
+
+def reverse_func(apps, schema_editor):
+    pass
+
 
 class Migration(migrations.Migration):
-
+    
     dependencies = [
         ('testsession', '0033_auto_20181217_1227'),
     ]
 
     operations = [
+        migrations.RunPython(forwards_func, reverse_func),
         migrations.AlterField(
             model_name='scenariocase',
             name='vng_endpoint',

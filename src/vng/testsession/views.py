54a41b0adb570c4a8e0e64c74fb4dc8961784e38
Alvise <alvise@maykinmedia.nl>
2019-01-14 11:13:00 +0100

    Ol-to-new in report log

--- src/vng/testsession/views.py
+++ src/vng/testsession/views.py
@@ -133,7 +133,7 @@ class SessionLogView(OwnerMultipleObjects):
 
     def get_queryset(self):
         print(self.kwargs)
-        return SessionLog.objects.filter(session__pk=self.kwargs['session_id']).order_by('-date')
+        return SessionLog.objects.filter(session__pk=self.kwargs['session_id']).order_by('date')
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)

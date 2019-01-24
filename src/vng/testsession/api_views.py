18655668b212b3e62dbfec993b29d78ac7c068a0
Alvise <alvise@maykinmedia.nl>
2019-01-24 17:10:38 +0100

    Remove Authentication Header

--- src/vng/testsession/api_views.py
+++ src/vng/testsession/api_views.py
@@ -145,6 +145,7 @@ class ExposedUrlView(generics.ListAPIView):
 class RunTest(CSRFExemptMixin, View):
     """ Proxy-view between clients and servers """
     error_codes = [(400, 599)]  # boundaries considered as errors
+    exclude_request_header = ['Authorization']
 
     def get_exposed_url(self):
         exposed_url = '{}/{}'.format(self.kwargs['exposed_url'], self.kwargs['name'])
@@ -194,6 +195,9 @@ class RunTest(CSRFExemptMixin, View):
             request_headers['Accept-Crs'] = request_headers.pop('HTTP_ACCEPT_CRS')
         if 'CONTENT_TYPE' in request_headers:
             request_headers['Content-Type'] = request_headers.pop('CONTENT_TYPE')
+        if not settings.DEBUG:
+            for rh in self.exclude_request_header:
+                del request_headers[rh]
 
         return request_headers
 

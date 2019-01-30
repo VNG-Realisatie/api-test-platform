211ec9168b632481c96f2783dab7a7f0c3c0b41d
Alex de Landgraaf <alex@maykinmedia.nl>
2019-01-30 17:51:24 +0100

    Fix missing authorization pass-through header

--- src/vng/testsession/api_views.py
+++ src/vng/testsession/api_views.py
@@ -145,7 +145,6 @@ class ExposedUrlView(generics.ListAPIView):
 class RunTest(CSRFExemptMixin, View):
     """ Proxy-view between clients and servers """
     error_codes = [(400, 599)]  # boundaries considered as errors
-    exclude_request_header = ['Authorization']
 
     def get_exposed_url(self):
         exposed_url = '{}/{}'.format(self.kwargs['exposed_url'], self.kwargs['name'])
@@ -198,10 +197,6 @@ class RunTest(CSRFExemptMixin, View):
             request_headers['Content-Crs'] = request_headers.pop('HTTP_CONTENT_CRS')
         if 'CONTENT_TYPE' in request_headers:
             request_headers['Content-Type'] = request_headers.pop('CONTENT_TYPE')
-        if not settings.DEBUG:
-            for rh in self.exclude_request_header:
-                if rh in request_headers:
-                    del request_headers[rh]
 
         return request_headers
 

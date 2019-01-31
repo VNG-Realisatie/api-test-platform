4d7c4ebb075bc7c2d3e909b4c7696e52f53d8fae
Alvise <alvise@maykinmedia.nl>
2019-01-31 12:53:54 +0100

    Transaction and custom error message

--- src/vng/servervalidation/api_views.py
+++ src/vng/servervalidation/api_views.py
@@ -1,13 +1,16 @@
 
 from django.utils import timezone
+from django.db import transaction
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Prefetch
 
 from rest_framework import permissions, viewsets
+from rest_framework.exceptions import bad_request
 from rest_framework.authentication import (
     SessionAuthentication, TokenAuthentication
 )
 
+from .exceptions import Error400
 from ..permissions.UserPermissions import *
 from .serializers import ServerRunSerializer
 from .models import (
@@ -25,17 +28,14 @@ class ServerRunViewSet(LoginRequiredMixin, viewsets.ModelViewSet):
             Prefetch('endpoint_set', to_attr='endpoints'),
             Prefetch('endpoint_set__test_scenario_url'))
 
+    @transaction.atomic
     def perform_create(self, serializer):
         server = serializer.save(user=self.request.user, pk=None, started=timezone.now())
         for ep in serializer._kwargs['data']['endpoints']:
             name = ep['name']
             url = ep['url']
-            # import pdb
-            # pdb.set_trace()
-            print(name, url)
             try:
-
                 tsu = TestScenarioUrl.objects.get(name=name, test_scenario=server.test_scenario)
                 Endpoint.objects.create(test_scenario_url=tsu, url=url, server_run=server)
             except Exception as e:
-                print(e, name, url)
+                raise Error400("The urls' names provided do not match")

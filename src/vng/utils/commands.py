b3d8f42f47f838baab37aa0b78373b7df65c3e4f
Joeri Bekker <joeri@maykinmedia.nl>
2019-02-27 14:00:04 +0000

    For further consumption by Alvise

--- src/vng/utils/commands.py
+++ src/vng/utils/commands.py
@@ -6,13 +6,14 @@ logger = logging.getLogger(__name__)
 
 
 def run_command(command):
-    logger.debug('running the COMMAND: %s', command)
+    logger.info('running the COMMAND: %s', command)
     my_env = os.environ.copy()
     my_env["CLOUDSDK_PYTHON"] = "/usr/bin/python2"
 
     if my_env.get("VIRTUAL_ENV"):
         del my_env["VIRTUAL_ENV"]
-
+    my_env["HOME"] = "/home/maykin"
+    logger.info('Environment: {}'.format(str(my_env)))
     subp = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=my_env)
     res, error = subp.communicate('n\n')
     if error:

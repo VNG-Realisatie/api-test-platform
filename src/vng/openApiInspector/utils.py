bdd04c7394db44836dfe5ff5d5db2bbbc8c38205
Alex de Landgraaf <alex@maykinmedia.nl>
2019-03-27 17:35:42 +0100

    Updating invalid version

--- src/vng/openApiInspector/utils.py
+++ src/vng/openApiInspector/utils.py
@@ -7,5 +7,8 @@ OpenAPIv = 3.0
 def openAPIInspector(url):
     resp = requests.get(url)
     data = resp.json()
-    version = float(data['openapi'][:3])
+    try:
+        version = float(data['openapi'][:3])
+    except Exception as e:
+        version = 0
     return version

a1629155ddb97cc19d997153089112e83c136a53
Alex de Landgraaf <alex@maykinmedia.nl>
2019-03-27 14:57:48 +0100

    Dutch translations, catch for invalid OpenAPI spec modified

--- src/vng/openApiInspector/utils.py
+++ src/vng/openApiInspector/utils.py
@@ -10,8 +10,5 @@ def openAPIInspector(url):
         data = resp.json()
         version = float(data['openapi'][:3])
     except Exception as e:
-        re.search('version: (([0-9]|.)+)', resp.text)
-        v = re.group(1)
-        if len(v.split('.') > 1):
-            version = float('{}.{}'.format(v.split[0], v.split[1]))
+        version = 0
     return version
--- src/vng/openApiInspector/views.py
+++ src/vng/openApiInspector/views.py
@@ -21,9 +21,9 @@ class OpenApiInspection(FormView):
             except Exception as e:
                 raise e
                 if isinstance(e, JSONDecodeError) or isinstance(e, AttributeError):
-                    form.add_error('url', u'The link provided does not contain a json schema')
+                    form.add_error('url', u'Deze link heeft geen geldige JSON schema')
                 else:
-                    form.add_error('url', u'The link provided is not reachable')
+                    form.add_error('url', u'Deze link is niet op te vragen')
                 return self.form_invalid(form)
             self.request.session['openapi'] = True if version >= 3 else False
             self.request.session['openapiv'] = version

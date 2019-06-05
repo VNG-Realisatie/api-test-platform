8bce854eb5fbd755850a414c432513e8286c5db2
Alvise <alvise@maykinmedia.nl>
2019-06-05 15:53:31 +0200

    Move to lower letter

--- src/vng/testsession/gemma_containers.py
+++ src/vng/testsession/gemma_containers.py
@@ -36,78 +36,78 @@ celery = Container(
 )
 
 NRC = Container(
-    name='NRC',
+    name='nrc',
     image='vngr/gemma-notifications',
     public_port=8004,
     private_port=8004,
     variables={
         'DB_HOST': 'localhost',
-        'DB_NAME': 'NRC',
+        'DB_NAME': 'nrc',
         'DB_USER': 'postgres',
         'DB_PASSWORD': 'postgres'
     }
 )
 
 ZTC = Container(
-    name='ZTC',
+    name='ztc',
     image='vngr/gemma-ztc',
     public_port=8002,
     private_port=8002,
     variables={
         'DB_HOST': 'localhost',
-        'DB_NAME': 'ZTC',
+        'DB_NAME': 'ztc',
         'DB_USER': 'postgres',
         'DB_PASSWORD': 'postgres'
     }
 )
 
 ZRC = Container(
-    name='ZRC',
+    name='zrc',
     image='vngr/gemma-zrc',
     public_port=8000,
     private_port=8000,
     variables={
         'DB_HOST': 'localhost',
-        'DB_NAME': 'ZRC',
+        'DB_NAME': 'zrc',
         'DB_USER': 'postgres',
         'DB_PASSWORD': 'postgres'
     }
 )
 
 BRC = Container(
-    name='BRC',
+    name='brc',
     image='vngr/gemma-brc',
     public_port=8003,
     private_port=8003,
     variables={
         'DB_HOST': 'localhost',
-        'DB_NAME': 'BRC',
+        'DB_NAME': 'brc',
         'DB_USER': 'postgres',
         'DB_PASSWORD': 'postgres'
     }
 )
 
 DRC = Container(
-    name='DRC',
+    name='drc',
     image='vngr/gemma-drc',
     public_port=8001,
     private_port=8001,
     variables={
         'DB_HOST': 'localhost',
-        'DB_NAME': 'DRC',
+        'DB_NAME': 'drc',
         'DB_USER': 'postgres',
         'DB_PASSWORD': 'postgres'
     }
 )
 
 AC = Container(
-    name='AC',
+    name='ac',
     image='vngr/gemma-autorisatiecomponent',
     public_port=8005,
     private_port=8005,
     variables={
         'DB_HOST': 'localhost',
-        'DB_NAME': 'AC',
+        'DB_NAME': 'ac',
         'DB_USER': 'postgres',
         'DB_PASSWORD': 'postgres'
     }

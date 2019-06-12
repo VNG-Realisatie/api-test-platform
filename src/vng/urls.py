c883685735963d31798eacc49a5c4169abc70fbe
Alvise <alvise@maykinmedia.nl>
2019-06-12 17:31:46 +0200

    General fixes

--- src/vng/urls.py
+++ src/vng/urls.py
@@ -1,9 +1,7 @@
 from django.conf import settings
 from django.conf.urls import include, url
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic.base import TemplateView
 from .decorators import anonymous_required
 from .base_url import *
